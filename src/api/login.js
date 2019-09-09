e48e914d147c746eee1f207efe9eb2e0eee6a455
yangwenbang <1471275556@qq.com>
2019-09-09 23:56:49 +0800

    xxx

--- src/api/login.js
+++ src/api/login.js
@@ -1,73 +1,61 @@
 import axios from '@/libs/api.request'
 
-export const carLogin = ({ userName, password}) => {
-  const data = {
-    userName,
-    password
-  }
-  return axios.request({
-    url: '/sys/admin/login',
-    data,
-    method: 'post'
-  })
+export const carLogin = ({ userName, password }) => {
+    const data = {
+        userName,
+        password
+    }
+    return axios.request({
+        url: '/sys/admin/login',
+        data,
+        method: 'post'
+    })
 }
 
 export const getWarehouse = (params) => {
-  return axios.request({
-    url: '/pwms/user/getWarehouse',
-    params: params,
-    method: 'get'
-  })
+    return axios.request({
+        url: '/pwms/user/getWarehouse',
+        params: params,
+        method: 'get'
+    })
 }
 
 export const getPlatform = (params) => {
-  return axios.request({
-    url: '/pwms/user/getPlatform',
-    params: params,
-    method: 'get'
-  })
+    return axios.request({
+        url: '/pwms/user/getPlatform',
+        params: params,
+        method: 'get'
+    })
 }
 
 export const getUserId = (params) => {
-  return axios.request({
-    url: '/pwms/user/getUserId',
-    params: params,
-    method: 'get'
-  })
+    return axios.request({
+        url: '/pwms/user/getUserId',
+        params: params,
+        method: 'get'
+    })
 }
 
 export const updatePassword = (params) => {
-  return axios.request({
-    url: '/sys/admin/updatePassword',
-    data: params,
-    method: 'post'
-  })
+    return axios.request({
+        url: '/sys/admin/updatePassword',
+        data: params,
+        method: 'post'
+    })
 }
 
 export const loginOut = (params) => {
-  return axios.request({
-    url: '/sys/admin/logout',
-    params: params,
-    method: 'get'
-  })
+    return axios.request({
+        url: '/sys/admin/logout',
+        params: params,
+        method: 'get'
+    })
 }
 
 export const warehouseChoose = (params) => {
-  return axios.request({
-    url: '/pwms/user/WarehouseChoose',
-    params: params,
-    method: 'put',
-    // transformRequest: [function (data) {
-    //   console.log(data)
-    //   let ret = ''
-    //   for (let it in data) {
-    //     ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it])
-    //   }
-    //   return ret
-    // }],
-    // headers: {
-    //   'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-    // }
-  })
+    return axios.request({
+        url: '/pwms/user/WarehouseChoose',
+        params: params,
+        method: 'put',
+    })
 }
-
--- src/view/ExampleView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>我是通用空白页面</div>
-</template>
-<script>
-export default {
-  name: 'ExampleView'
-}
-</script>
