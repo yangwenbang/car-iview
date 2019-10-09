a63ace2bea2bef3b953069bf1cb109e754c787f2
yangwenbang <1471275556@qq.com>
2019-10-09 10:44:35 +0800

    xxx

--- src/api/common.js
+++ src/api/common.js
@@ -3,7 +3,7 @@ import axios from '@/libs/api.request'
 //首页汇总接口
 export const queryIndexCount = (params) => {
     return axios.request({
-        url: '/',
+        url: '/index',
         params: params,
         method: 'get'
     })
--- src/view/home/home.vue
+++ src/view/home/home.vue
@@ -2,7 +2,7 @@
   <div>
     <Row :gutter="20">
       <i-col :xs="12" :md="8" :lg="8" v-for="(infor, i) in inforCardData" :key="`infor-${i}`" style="height: 120px;padding-bottom: 10px;">
-        <infor-card shadow :color="infor.color" :icon="infor.icon" :icon-size="36" :link="infor.link">
+        <infor-card v-if="!infor.hide" shadow :color="infor.color" :icon="infor.icon" :icon-size="36" :link="infor.link">
           <count-to :end="infor.count" count-class="count-style" />
           <p>{{ infor.title }}</p>
         </infor-card>
@@ -15,42 +15,87 @@
 import InforCard from "_c/info-card";
 import CountTo from "_c/count-to";
 import { queryIndexCount } from '@/api/common'
+import { setToken, getToken } from '@/libs/util'
+
 export default {
     name: "home",
+
     components: {
         InforCard,
         CountTo
     },
+
     data() {
         return {
-            inforCardData: [
-                {
-                    title: "已上架商品",
-                    icon: "ios-car",
-                    count: 0,
-                    color: "#2d8cf0",
-                    link: "/commodity/CommodityList"
-                },
-                {
-                    title: "未上架商品",
-                    icon: "md-log-out",
-                    count: 0,
-                    color: "#19be6b",
-                    link: "/commodity/CommodityList"
-                }
+
+          hideInIndex: true,
+
+          inforCardData: [
+              {
+                  title: "总商品数",
+                  icon: "ios-car",
+                  count: 0,
+                  color: "#2d8cf0",
+                  link: "/commodity/CommodityList",
+              },
+              {
+                  title: "待上架商品数",
+                  icon: "ios-car-outline",
+                  count: 0,
+                  color: "#19be6b",
+                  link: "/commodity/CommodityList",
+              },
+              {
+                  title: "待退款订单数",
+                  icon: "ios-list-box-outline",
+                  count: 0,
+                  color: "#f5222d",
+                  link: "/order/OrderList",
+              },
+              {
+                  title: "今日新增终端用户数",
+                  icon: "md-people",
+                  count: 0,
+                  color: "#fa541c",
+                  link: "/user/UserList",
+              },
+              {
+                  title: "质检商家用户数",
+                  icon: "ios-speedometer-outline",
+                  count: 0,
+                  color: "#eb2f96",
+                  link: "/qualityShop/shopList",
+              },
+              {
+                  title: "终端用户数",
+                  icon: "ios-people",
+                  count: 0,
+                  color: "#fa8c16",
+                  link: "/user/UserList",
+              }
             ]
-        };
+        }
     },
+
     mounted() {
-    
+      this.hideInIndex = (JSON.parse(this.$store.state.user.token).isAdmin == 1 ? false : true);
+      this.getCountFun();
     },
     methods:{
       getCountFun() {
         queryIndexCount().then(res => {
             if (res.data.code == "200") {
                 const countInfor = res.data.data;
-                this.inforCardData[0].count = countInfor.inEnterCount;
-                this.inforCardData[1].count = countInfor.outEnterCount;
+                this.inforCardData[0].count = countInfor.commoditys;
+                this.inforCardData[1].count = countInfor.waitingShelfCommoditys;
+                this.inforCardData[2].count = countInfor.waitingRefunds;
+                this.inforCardData[3].count = countInfor.currentDayAppUsers;
+                this.inforCardData[4].count = countInfor.merchantsUsers;
+                this.inforCardData[5].count = countInfor.appUsers;
+
+                this.inforCardData[3].hide = this.hideInIndex;
+                this.inforCardData[4].hide = this.hideInIndex;
+                this.inforCardData[5].hide = this.hideInIndex;
             } else {
                 this.$Message.error(res.data.msg);
             }
