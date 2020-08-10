dce8120ea9571c38e0bed06817fc53ebae77f541
yangwenbang <1471275556@qq.com>
2020-08-10 23:10:37 +0800

    xxx

---
+++ src/view/official/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main">
+    <div>
+      xxx
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'OfficialIndex',
+  components: {},
+  data () {
+    return {}
+  },
+  created () {
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="less" scoped>
+.main {
+    overflow-x: hidden;
+    height:100%;
+    overflow: auto;
+    padding: 15px;
+    width: 100%;
+}
+
+</style>
