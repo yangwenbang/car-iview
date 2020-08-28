f18a54ca3467c5888cb2480d361f51589e5f968d
yangwenbang <1471275556@qq.com>
2020-08-28 23:52:05 +0800

    xxx

--- src/view/official/Index.vue
+++ src/view/official/Index.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="main">
     <div>
-      xxx
+		<div style="float: right; margin-right: 20%; font-size: 16px;"><span style="margin-right: 50px;">首页/home</span><span style="margin-right: 50px;">商家入驻/stationed</span><span style="margin-right: 50px;">关于/about</span><span style="margin-right: 50px;">联系/contact</span></div>
+    <div class="bg-img"></div>
     </div>
   </div>
 </template>
@@ -28,5 +29,21 @@ export default {
     padding: 15px;
     width: 100%;
 }
-
+.login {
+    width: 100%;
+    height: 100%;
+    background-image: url('../../assets/images/v2_pvwxm2.png');
+    background-repeat: no-repeat;
+    background-position: center;
+    position: relative;
+    background-color:rgba(251, 198, 71, 1);
+    @media screen and (max-width: 600px) {
+        .top {
+            padding-top: 120px !important;
+        }
+    }
+    .top {
+        padding-top: 200px;
+    }
+}
 </style>
